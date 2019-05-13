666da8d1d83733b195c9b247246fc3c7a38ada5b
Jacob Johnston <jacob.johnston.2022@student.lhschools.org>
2019-05-13 14:18:48 -0400

    Started project

---
+++ src/MorseCode.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class MorseCode {
+
+    public static void main (String [] args){
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+
+
+        String Translation;
+
+
+
+        System.out.println("Welcome to the Morse Code to English and English to Morse Code translator");
+        System.out.println("To go from English to Morse Code please type 'english'");
+        System.out.println("To go from Morse Code to English please type 'morse'");
+        Translation = keyboard.nextLine();
+        }
+        public static String Translation2 (String ){
+
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
